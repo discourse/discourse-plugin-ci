bbc62ef5aa210c6068cc6d1b1e5aeac666e5ec6b
David Taylor <david@taylorhq.com>
2018-10-12 23:29:12 +0100

    FEATURE: Skip JS/Ruby if there are no tests/specs for this plugin

--- script.sh
+++ script.sh
@@ -5,11 +5,19 @@ chmod -R 777 . # This is necessary if your plugin installs gems
 
 echo "travis_fold:start:starting_docker_container"
 
+[ -d './test' ] || RUBY_ONLY=1
+[ -d './spec' ] || JS_ONLY=1
+
+echo "RUBY_ONLY=$RUBY_ONLY"
+echo "JS_ONLY=$JS_ONLY"
+
 docker run \
   -t \
   -e "COMMIT_HASH=origin/tests-passed" \
   -e "SKIP_CORE=1" \
-  -e TRAVIS=$TRAVIS \
+  -e RUBY_ONLY \
+  -e JS_ONLY \
+  -e TRAVIS \
   -e SINGLE_PLUGIN=$plugin_name \
   -v $(pwd):/var/www/discourse/plugins/$plugin_name \
   discourse/discourse_test:release
